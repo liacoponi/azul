baddc7e7c73d98c493a05fe864fac95712710182
liacoponi <jacoponi@gmail.com>
2023-02-07 14:20:29 -0500

    90% of logic in place

--- azul/definitions.py
+++ azul/definitions.py
@@ -13,18 +13,17 @@ class Bag:
     def pick_from_bag(self):
         if len(self.tiles) < 4:
             # TODO: should take then refill
-            self.fill_bag()
-        return [self.tiles.pop() for _ in range(4]
+            self.refill_bag()
+        return [self.tiles.pop() for _ in range(4)]
 
 
 class Factory:
     def __init__(self, no_of_players):
         # 2 player -> 5 displays, 3 -> 7, 4 -> 9
-        self.no_of_displays = no_of_players*2 + 1
+        self.no_of_displays = no_of_players * 2 + 1
         self.displays = [None for _ in range(self.no_of_displays)]
         self.center = []
         self.center = ['1']
-
 
     def pick_from_display(self, display_num, color):
         tiles_num = self.count(display_num, color)
@@ -37,7 +36,7 @@ class Factory:
 
 class Wall:
     def __init__(self):
-        self.wall = [[None * 5][None * 5]]
+        self.wall = [['' * 5]['' * 5]]
 
     def add_tile(self, tile_color, row_number):
         self.wall[row_number][0] = 0  # TODO
@@ -47,7 +46,6 @@ class PatternLine:
     def __init__(self, size):
         self.line_color = None
         self.tiles_left = size
-
 
     def fill_line(self, color, tiles_to_add):
         if not self.tiles:
@@ -65,26 +63,34 @@ class PatternLine:
 class Game:
     def __init__(self, players):
         self.players = players
-        self.factory = Factory(player_no = len(players))
+        self.player_no = len(players)
+        self.factory = Factory(self.player_no)
         self.bag = Bag()
         self.first_player_id = 0
         self.games_counter = 0
-
+        self.is_last_round = 0
 
     def new_game(self):
-        self.factory =
+        self.factory = Factory(self.player_no)
         self.bag.refill_bag()
         self.games_counter += 1
         self.pick_first_player()
 
-
     def pick_first_player(self, number_of_players):
         self.first_player_id = random.randint(0, number_of_players)
 
-
     def turn_reset(self):
-        for factory in self.factory:
-            self.bag.pick_from_bag(4)
+        # reset factory displays and center
+        for i in range(self.factory.displays):
+            self.factory.displays[i] = self.bag.pick_from_bag(4)
+        self.factory.center = ['1']
+        # reset players' pattern tiles and floor
+        for player in self.players:
+            player.reset_lines()
+
+    def is_last_turn(self):
+        if not self.factory.center and not any(self.factory.displays):
+            return True
 
 
 class Player:
@@ -94,14 +100,16 @@ class Player:
         self.floor_tiles = []
         self.wall = Wall()
         self.pattern_lines = []
+        self.victory_points_last_turn = 0
+        self.has_finished_row = 0
 
-
-    def reset_board(self):
-        # reset board at the end of each turn
-        self.wall.clear_wall()
+    def reset_lines(self):
         self.pattern_lines = [PatternLine(size) for size in range(5)]
         self.floor_tiles = []
 
+    def reset_board(self):
+        self.reset_lines()
+        self.wall.clear_wall()
 
     def pick_tiles(self, display, color, pattern_line_row):
         picked_tiles = factory.pick(color, display)
@@ -120,11 +128,14 @@ class Player:
         for row_number, row in enumerate(self.floor_tiles):
             row_color = row[0]
             if None not in row:
-                # row is complete, move tile and add points
-                self.victory_points += self.Wall.add(row_color, row_number)
+                # pattern line is complete, move tile and add points
+                self.victory_points += self.wall.add(row_color, row_number)
+                # completed row in wall, end game
+                if None not in self.wall.row[row_number]:
+                    self.has_finished_row = True
 
-        # Reset floor tiles and pattern lines
-        self.floor_tiles = []
-        self.pattern_lines = []
+        self.victory_points_last_turn = victory_points_before - self.victory_points
 
-        return victory_points_before - self.victory_points
+    def score_final_vp(self):
+        # ToDo: finis method and add score breakdown
+        return 10
--- azul/run.py
+++ azul/run.py
@@ -1,11 +1,8 @@
-import csv
-import random
-import os
 import definitions
 
 
 def main(no_of_players):
-    players = [definitions.Player() for p in no_of_players]
+    players = [definitions.Player(i) for i in range(no_of_players)]
     game = definitions.Game(players)
     # continuously play new games until KeyboardInterrupt
     while True:
@@ -18,12 +15,27 @@ def main(no_of_players):
 def play(game):
     """ Play a game of Azul """
     # reset bag and factory, pick first player, etc.
-    game.set_up()
+    game.new_game()
     print("\n** Game Starts **")
-    # turn starts, reset player boards
-    game.turn_reset()
-    # reset pattern_lines, just in case
+    while not game.is_last_round:
+        # turn starts, reset players' boards
+        game.turn_reset()
+        player_turn_id = game.first_player_id - 1
 
+        # each player plays a turn until there are no tiles left
+        while not game.is_last_turn():
+            player_turn_id = (player_turn_id + 1) % game.player_no
+            print("Player's turn: " + str(player_turn_id))
+            game.players[player_turn_id].pick_tiles()
+
+        for player in game.players:
+            player.turn_end()
+            if player.has_finished_row:
+                game.is_last_round = True
+
+    # End of game scoring
+    for player in game.players:
+        player.score_final_vp()
 
 
 if __name__ == '__main__':
